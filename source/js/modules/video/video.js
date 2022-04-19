de1685a9d4fa6e4fd4be1ae6cdafeada362ce052
Andrew <korotkov915@gmail.com>
2022-04-19 23:02:29 +0300

    FIX: video settings

--- source/js/modules/video/video.js
+++ source/js/modules/video/video.js
@@ -1,6 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const getVideo = () => {
   const controlVideo = document.querySelector('.gym__video-control');
   let player;
+  const tag = document.createElement('script');
+  tag.src = 'https://www.youtube.com/iframe_api';
+  const firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
   controlVideo.addEventListener('click', function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
       height: '100%',
@@ -9,6 +16,8 @@ const getVideo = () => {
       playerVars: {
         'autoplay': 0,
         'controls': 1,
+        'modestbranding': 1,
+        'showinfo': 0,
       },
       events: {
         'onReady': onPlayerReady,
