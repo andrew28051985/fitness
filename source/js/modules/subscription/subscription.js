0b45a95ca7cfad5e7d13cace9b479e8ccb47ba9d
Andrew <korotkov915@gmail.com>
2022-04-23 19:00:53 +0300

    ADD: click button on card

--- source/js/modules/subscription/subscription.js
+++ source/js/modules/subscription/subscription.js
@@ -14,14 +14,17 @@ const onCardSwitch = () => {
     case '1':
       isActiveCard.classList.remove('card--active');
       cards[0].classList.add('card--active');
+      onClickButtonCard();
       break;
     case '6':
       isActiveCard.classList.remove('card--active');
       cards[1].classList.add('card--active');
+      onClickButtonCard();
       break;
     case '12':
       isActiveCard.classList.remove('card--active');
       cards[2].classList.add('card--active');
+      onClickButtonCard();
       break;
   }
 };
@@ -48,30 +51,51 @@ const switchTabs = () => {
 };
 
 let count = 0;
-const onClickCard = (card) => {
-  console.log(count);
-  const button = card.querySelector('.btn');
-  if (count === 0) {
-    button.addEventListener('click', (evt) => {
-      evt.preventDefault();
-      count++;
-    });
-  } else {
-    button.addEventListener('click', () => {
+let oldButton;
+let oldPrice;
+const countButtonClick = (evt) => {
+  const isButton = evt.target.classList.contains('btn');
+  if (isButton && count === 0) {
+    oldButton = evt.target.parentNode.childNodes[1].textContent;
+    oldPrice = evt.target.parentNode.childNodes[5].textContent.replace(/[^0-9]/g, '');
+    evt.preventDefault();
+    count++;
+  } else if (!isButton && count === 0) {
+    count = 1;
+  } else if (isButton && count === 1) {
+    const newButton = evt.target.parentNode.childNodes[1].textContent;
+    const newPrice = evt.target.parentNode.childNodes[5].textContent.replace(/[^0-9]/g, '');
+    if ((oldButton === newButton) && (oldPrice === newPrice)) {
       count = 0;
-    });
+    } else {
+      oldButton = evt.target.parentNode.childNodes[1].textContent;
+      oldPrice = evt.target.parentNode.childNodes[5].textContent.replace(/[^0-9]/g, '');
+      evt.preventDefault();
+    }
   }
 };
+
 const onClickButtonCard = () => {
-  console.log(isMobile);
   if (isMobile) {
     cards.forEach((card) => {
       if (card.classList.contains('card--active')) {
-        card.addEventListener('click', onClickCard(card));
+        const items = card.querySelectorAll('.card__item');
+        items.forEach((item) => {
+          const button = item.querySelector('.btn');
+          item.addEventListener('click', countButtonClick);
+        });
       }
     });
   } else {
-
+    cards.forEach((card) => {
+      if (card.classList.contains('card--active')) {
+        const items = card.querySelectorAll('.card__item');
+        items.forEach((item) => {
+          const button = item.querySelector('.btn');
+          item.removeEventListener('click', countButtonClick);
+        });
+      }
+    });
   }
 };
 
@@ -90,7 +114,6 @@ breakpoint.addListener(breakpointChecker);
 
 const subscriptionCard = () => {
   switchTabs();
-  onClickButtonCard();
 };
 
 export {subscriptionCard, breakpointChecker};
