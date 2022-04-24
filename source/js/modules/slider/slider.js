dd093fc153cc09331dcda83e1398c05add183326
Andrew <korotkov915@gmail.com>
2022-04-24 20:59:01 +0300

    ADD: full section soaches

--- source/js/modules/slider/slider.js
+++ source/js/modules/slider/slider.js
@@ -1,15 +1,32 @@
 import Swiper from '../../vendor/swiper';
 
 const slider = () => {
-  const swiper = new Swiper('.mySwiper', {
+  const swiper = new Swiper('.mySwiper-coaches', {
     slidesPerView: 4,
     spaceBetween: 40,
     slidesPerGroup: 1,
     loop: true,
     loopFillGroupWithBlank: true,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.slider__button-next',
+      prevEl: '.slider__button-prev',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      // when window width is >= 1200px
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
     },
   });
 
